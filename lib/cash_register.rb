e20a722ee2919c4c7ead6cfc2e10cd921af8cc2d
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2020-01-16 08:17:56 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,37 @@
+class CashRegister
+  
+  @@items
+  
+  attr_accessor :total
+  
+  def initialize(total = 0)
+    @total = total
+    @employee_discount = 20%
+    @@items << item
+  end
+  
+  def total
+     cash_register.total = 100
+   end
+   
+  def add_item(item) 
+   cash_register.add_item = cash_register.total + cash_register.add_item
+  end
+  
+  def apply_discount(items)
+    items.each do |item|
+    if @employee_discount applies
+    puts "success your updated @total = @total - @employee_discount"
+  else
+    puts "no discount to apply"
+  end
+end
 
+def items
+  @@items
+  end
+  
+  def void_last_transaction
+    @total = @total - last item
+    end
+end
